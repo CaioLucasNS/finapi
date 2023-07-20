2edad621c6ccefed22ee8e4e276651f6d70f4fab
Caio Lucas <lcaio1281@gmail.com>
2023-07-19 23:02:28 -0300

    Cadastro de conta

--- src/index.js
+++ src/index.js
@@ -1,37 +1,31 @@
 const express = require("express");
+const { v4: uuidv4 } = require("uuid");
 
 const app = express();
 
-// middleware para entender JSON
+// Middleware
 app.use(express.json());
 
-app.get("/courses", (request, response) => {
-  const query = request.query;
-  console.log("query: ", query);
+const customers = [];
 
-  return response.json(["Curso 1", "Curso 2", "Curso 3"]);
-});
+/**
+ * cpf - string
+ * name - string
+ * id - uuid
+ * statement []
+ */
+app.post("/account", (request, response) => {
+  const { cpf, name } = request.body;
+  const id = uuidv4();
 
-app.post("/courses", (request, response) => {
-  const body = request.body;
-  console.log("body: ", body);
+  customers.push({
+    cpf,
+    name,
+    id,
+    statement: [],
+  });
 
-  return response.json(["Curso 1", "Curso 2", "Curso 3", "Curso 4"]);
-});
-
-app.put("/courses/:id", (request, response) => {
-  const { id } = request.params;
-  console.log("id: ", id);
-
-  return response.json(["Curso 6", "Curso 2", "Curso 3", "Curso 4"]);
-});
-
-app.patch("/courses/:id", (request, response) => {
-  return response.json(["Curso 6", "Curso 7", "Curso 3", "Curso 4"]);
-});
-
-app.delete("/courses/:id", (request, response) => {
-  return response.json(["Curso 6", "Curso 7", "Curso 4"]);
+  return response.status(201).send();
 });
 
 app.listen(3333);
